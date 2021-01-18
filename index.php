3f406ab27b48c9e3aec62ec2f6b3520cc29dbc23
HalaHyasat <71769566+HalaHyasat@users.noreply.github.com>
2021-01-18 20:44:42 +0200

    My answers

--- index.php
+++ index.php
@@ -12,28 +12,24 @@ echo ucfirst ("Q(1).C: ".$string1)."<br>";
 echo ucwords ("Q(1).D: ".$string1)."<br>";
 
 //Question 2
-$string2 = str_split("082307", "2");
-$newString2 = implode(":", $string2);
-echo "Q(2): ".$newString2."<br>";
+$string2 = implode(":", str_split("082307", "2"));
+echo "Q(2): ".$string2."<br>";
 
 //Question 3
-$string3 = "he quick brown fox jumps over the lazy dog";
-if(strchr($string3,"jumps"))
+if(strchr("he quick brown fox jumps over the lazy dog","jumps"))
 {echo "Q(3): "."The word exist in the sentence"."<br>";}
 else 
 {echo "Q(3): "."the word don't exist in the sentence"."<br>";}
 
 //Question 4
-$string4 = 'www.example.com/public_html/index.php';
-$newString4= substr(strrchr($string4, "/"), 1);
-echo "Q(4): ".$newString4."<br>";
+$string4= substr(strrchr("www.example.com/public_html/index.php", "/"), 1);
+echo "Q(4): ".$string4."<br>";
 
 //Question 5
 echo strtok("Q(5): "."Orange@example.com", "@")."<br>";
 
 //Question 6
-$string6 ='Orange@example.com';
-echo "Q(6): ".substr ($string6 ,-3)."<br>";
+echo "Q(6): ".substr ("Orange@example.com" ,-3)."<br>";
 
 //Question 7
 function passwordEncrypt($string7)
@@ -42,21 +38,17 @@ return substr(str_shuffle($password), 0, $string7);}
 echo "Q(7): ".passwordEncrypt(8)."<br>";
 
 //Question 8
-$string8 =  'the quick brown fox jumps over the lazy dog';
-echo "Q(8): ".str_replace('the', 'That',$string8)."<br>";
+echo "Q(8): ".str_replace('the', 'That',"the quick brown fox jumps over the lazy dog")."<br>";
 
 //Question 9
-$string9 = 'football';
-$string99 = 'footboll';
-$str_pos = strspn($string9 ^ $string99, "\0");
+$string9 = strspn("football" ^ "footboll", "\0");
 printf("Q(9): ".'First difference between two strings at position %d: "%s" vs "%s"'."<br>",
-$str_pos, $string9[$str_pos], $string99[$str_pos]);
+$string9, "football"[$string9], "footboll"[$string9]);
 
 //Question number 10
 $string10 = "Twinkle, twinkle, little star,\nHow I wonder what you are.\nUp above the world so high,\nLike a diamond in the sky.";
-$Arr10 = explode("\n", $string10);
 echo "Q(10): ";
-var_dump($Arr10);
+var_dump(explode("\n", $string10));
 echo "<br>";
 
 //Question no.11
@@ -67,12 +59,10 @@ if (strlen($newString11) > 1)
 echo "Q(11): ".$newString11."<br>";
 
 //Question 12
-$string12 = 'orange@example.com';
-echo "Q(12): ".substr($string12, 6)."<br>";
+echo "Q(12): ".substr("orange@example.com", 6)."<br>";
 
 //Question 13
-$string13 = 'The brown fox';
-echo "Q(13): ".substr_replace($string13, 'quick ', 4, 0)."<br>";
+echo "Q(13): ".substr_replace("The brown fox", 'quick ', 4, 0)."<br>";
 
 //Question 14
 echo "Q(14): ".substr("The quick brown fox",0,3)."<br>";
@@ -88,11 +78,10 @@ if (substr($string16, 0, strlen($sub_string)) == $sub_string)
 echo "Q(16): ".$string16."<br>";
 
 //Question no.17
-$string17 ='The quick brown fox jumps over the lazy dog///';
-echo "Q(17): ".trim($string17 , '///')."<br>";
+echo "Q(17): ".trim("The quick brown fox jumps over the lazy dog///" , '///')."<br>";
 
 //Question 18
-$string18 = 'http://www.example.com/5478631';
+$string18 = "http://www.example.com/5478631";
 echo "Q(18): ".substr($string18, strrpos($string18, '/' )+1)."<br>";
 
 //Question 19
